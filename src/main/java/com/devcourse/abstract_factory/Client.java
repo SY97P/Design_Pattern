630bebf7043f4be40296a5778e5e42f9d9d9e68d
SY97P <onetuks@naver.com>
2023-08-09 18:57:41 +0900

    feat: Abstract Factory 패턴 수정

--- src/main/java/com/devcourse/abstract_factory/Client.java
+++ src/main/java/com/devcourse/abstract_factory/Client.java
@@ -6,22 +6,17 @@ import com.devcourse.abstract_factory.module.factory.JapanMcDonaldFactory;
 import com.devcourse.abstract_factory.module.factory.KoreanMcDonaldFactory;
 import com.devcourse.abstract_factory.module.factory.McDonaldFactory;
 
-import java.util.Optional;
-
 public class Client {
     public static void main(String[] args) {
-        Optional<Order> order = getOrder(Nation.CN);
-        order.ifPresent(Order::cook);
+        Order order = makeOrder(Nation.KR);
+        order.cook();
     }
 
-    private static Optional<Order> getOrder(Nation nation) {
-        if (nation.equals(Nation.KR)) {
-            McDonaldFactory factory = new KoreanMcDonaldFactory();
-            return Optional.of(new Order(factory));
-        } else if (nation.equals(Nation.JP)) {
+    private static Order makeOrder(Nation nation) {
+        if (nation.equals(Nation.JP)) {
             McDonaldFactory factory = new JapanMcDonaldFactory();
-            return Optional.of(new Order(factory));
+            return new Order(factory);
         }
-        return Optional.empty();
+        return new Order(new KoreanMcDonaldFactory());
     }
 }
